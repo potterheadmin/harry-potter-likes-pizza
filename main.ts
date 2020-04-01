d99291c1cc656f4ae837debad189f07feb9e4298
potterheadmin <62974618+potterheadmin@users.noreply.github.com>
2020-03-31 22:31:51 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,43 @@
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    pizza.setPosition(Math.randomRange(0, 160), Math.randomRange(0, 120))
+    info.startCountdown(5)
+})
+let pizza: Sprite = null
+scene.setBackgroundColor(8)
+let mySprite = sprites.create(img`
+. . f f f f f f f f f f . . . 
+. f f f f f f f f f f f f f . 
+f f f f f f f 4 4 f f f f f . 
+f f f f 4 4 2 4 4 f f f f f f 
+f f f 4 4 4 2 2 4 4 f f f f f 
+f f f 4 4 4 4 2 4 4 4 4 f f f 
+f f f f f f f 4 f f f f f f f 
+f f f f 7 4 f 4 f 7 4 f f f f 
+f f 4 f 7 4 f f f 7 4 f 4 f f 
+. f 4 f f f f 4 f f f f 4 4 f 
+. f f 4 4 4 4 4 4 4 4 4 4 4 f 
+. . f 4 4 f 4 4 4 f 4 4 4 f . 
+. . f f 4 f f f f f 4 4 f f . 
+. . . f f 4 4 4 4 4 4 f f . . 
+. . . . f f f f f f f f . . . 
+`, SpriteKind.Player)
+controller.moveSprite(mySprite)
+pizza = sprites.create(img`
+. . . . . . b b b b . . . . . . 
+. . . . . . b 4 4 4 b . . . . . 
+. . . . . . b b 4 4 4 b . . . . 
+. . . . . b 4 b b b 4 4 b . . . 
+. . . . b d 5 5 5 4 b 4 4 b . . 
+. . . . b 3 2 3 5 5 4 e 4 4 b . 
+. . . b d 2 2 2 5 7 5 4 e 4 4 e 
+. . . b 5 3 2 3 5 5 5 5 e e e e 
+. . b d 7 5 5 5 3 2 3 5 5 e e e 
+. . b 5 5 5 5 5 2 2 2 5 5 d e e 
+. b 3 2 3 5 7 5 3 2 3 5 d d e 4 
+. b 2 2 2 5 5 5 5 5 5 d d e 4 . 
+b d 3 2 d 5 5 5 d d d 4 4 . . . 
+b 5 5 5 5 d d 4 4 4 4 . . . . . 
+4 d d d 4 4 4 . . . . . . . . . 
+4 4 4 4 . . . . . . . . . . . . 
+`, SpriteKind.Food)
